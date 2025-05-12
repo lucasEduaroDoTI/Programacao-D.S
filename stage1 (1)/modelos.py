c61dd6bcc2a6fc644ec96431099ec2e101c6a8fc
lucasEduaroDoTI <164202873+lucasEduaroDoTI@users.noreply.github.com>
2025-05-12 10:49:42 -0300

    Update modelos.py

--- stage1 (1)/modelos.py
+++ stage1 (1)/modelos.py
@@ -1,46 +1,37 @@
-class Filme:
+class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         self.__nome = nome.title()
         self.ano = ano
         self.duracao = duracao
         self.__likes = 0
 
-    @property
-    def likes(self):
-        return self.__likes
-
-    def dar_likes(self):
-        self.__likes += 1
-
-    @property
-    def nome(self):
-        return self.__nome
-
-    @nome.setter
-    def nome(self, nome):
-        self.__nome = nome
-
-class Serie:
+class Serie(Programa):
     def __init__(self, nome, ano, temporadas):
         self.__nome = nome.title()
         self.ano = ano
-        self.temporadas = temporadas
+        self.temporada = temporadas
         self.__likes = 0
 
-    @property
-    def likes(self):
-        return self.__likes
+class Programa:
+    def __init__(self, nome, ano):
+        self.__nome = nome.title()
+        self.ano = ano
+        self.__like = 0
 
-    def dar_likes(self):
-        self.__likes += 1
+        @property
+        def likes(self):
+            return self.__likes
 
-    @property
-    def nome(self):
-        return self.__nome
+        def dar_likes(self):
+            self.__likes += 1
 
-    @nome.setter
-    def nome(self, nome):
-        self.__nome = nome
+        @property
+        def nome(self):
+            return self.__nome
+
+        @nome.setter
+        def nome(self, novo_nome):
+            self.__nome = novo_nome.title()
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
 print(vingadores.nome)
